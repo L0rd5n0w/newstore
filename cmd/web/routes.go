2818a6481ab38f65dea71a66585b6c4737160276
Adewusi Ifeoluwa Daniel <ifeoluwaadewusi04@gmail.com>
2026-07-20 18:45:36 +0100

    Broken URl fixes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,10 +8,10 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", app.home)
-	mux.HandleFunc("GET /book/view/:id", app.view)
+	mux.HandleFunc("GET /book/view/{id}", app.view)
 	mux.HandleFunc("GET /form", app.form)
 	mux.HandleFunc("POST /form/post", app.formHandler)
-	mux.HandleFunc("GET /form/update/:id", app.formUpdate)
+	mux.HandleFunc("GET /form/update/{id}", app.formUpdate)
 	mux.HandleFunc("POST /form/update", app.formUpdateSaver)
 	mux.HandleFunc("DELETE /book/delete", app.delete)
 
